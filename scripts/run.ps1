53c4395d0d2a1f0dbc6dfce411c97333162c75ea
Milan Unger <30626125+milung@users.noreply.github.com>
2023-10-18 09:39:37 +0200

    Milan/devcontainer (#2)
    * override command
    
    * removal of mount point
    
    * start scripts and container lifecycle
    
    ---------
    
    Co-authored-by: Milan Unger <nilan.unger@siemens-healthineers.com>

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -15,6 +15,16 @@ switch ($command) {
         docker run -it --rm -v ${ProjectRoot}/book-src/:/usr/src/app/book-src/ -p 3380:3380 milung/book-builder:latest
     }
 
+    "devc-start" {
+        $location = Get-Location
+        try {
+            cd /usr/src/app
+            npm run serve-spa
+        } finally {
+            Set-Location $location
+        }
+    }
+
     "build" {
         docker build -f ${ProjectRoot}/build/docker/Dockerfile -t milung/book-builder:local ${ProjectRoot} 
     }
